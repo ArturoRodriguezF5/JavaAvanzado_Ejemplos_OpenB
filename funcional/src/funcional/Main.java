39f11b3311b097fbecb33c0276aef6edc7073a50
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-04-13 21:35:28 -0600

    Commit para actualizar cambios en el ejemplo de programación funcional.

--- funcional/src/funcional/Main.java
+++ funcional/src/funcional/Main.java
@@ -1,15 +1,16 @@
 package funcional;
 
 public class Main {
-    private static int contador = 0;
+    //private static int contador = 0;
     public static void main(String[] args) {
-
+        // Se invoca a la función pura, una función pura es aquella que siempre produce la misma salida.
         int res1 = suma(5, 2);
         int res2 = suma(5, 2);
         System.out.println(res1 + " " + res2);
     }
     public static int suma(int a, int b) {
-        contador++;
+        // Si el resultado depende de algo fuera de la fucnion, no es PURA.
+        //contador++;
         return a + b;
     }
     public static int multiplicacion(int a, int b) {
