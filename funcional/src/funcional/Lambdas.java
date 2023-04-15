5a2feed9c14ea23b426018ee9849bb351663ec74
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-04-14 20:55:10 -0600

    Commit para actualizar cambios en el ejemplo de programación funcional.

--- funcional/src/funcional/Lambdas.java
+++ funcional/src/funcional/Lambdas.java
@@ -1,11 +1,12 @@
 package funcional;
 
 import java.util.ArrayList;
-import java.util.Locale;
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class Lamdas {
+public class Lambdas {
     private static Function<String, String> toMayus = x -> x.toUpperCase();
     private static Function<Integer, String> toStringg = x -> x.toString();
     public static void main(String[] args) {
@@ -18,11 +19,19 @@ public class Lamdas {
         nombres.add("Arturo");
         nombres.add("Katya");
         nombres.add("Jenny");
+        nombres.add("Ariana");
 
-        Stream<String> valores = nombres.stream().map(x -> x.toUpperCase());
+        Stream<String> valores = nombres.stream().map(x -> x.toUpperCase())
+                .filter(x -> x.startsWith("A") && x.endsWith("A"));
         valores.forEach(x -> System.out.println(x));
-    }
-    public static String diHola(String nombre) {
-        return "Hola: " + nombre;
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        IntStream inNums = Arrays.stream(numbers);
+        int res =  inNums.filter(x -> x % 2 == 0).reduce(0, (x, y) -> {
+            System.out.println("X es: " + x + " Y es: " + y);
+            return x + y;
+        });
+        System.out.println("Mi suma de pares es: " + res);
+
     }
 }
