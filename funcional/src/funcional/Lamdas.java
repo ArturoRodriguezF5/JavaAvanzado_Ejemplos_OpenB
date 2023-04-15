9edf3aa5a8c3cf6eb0a97686ec8b854d0845d3f9
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-04-14 20:04:45 -0600

    Commit para actualizar cambios en el ejemplo de programación funcional.

--- funcional/src/funcional/Lamdas.java
+++ funcional/src/funcional/Lamdas.java
@@ -1,7 +1,9 @@
 package funcional;
 
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Lamdas {
     private static Function<String, String> toMayus = x -> x.toUpperCase();
@@ -10,6 +12,15 @@ public class Lamdas {
 
         System.out.println(toMayus.apply("Arturo"));
         System.out.println(toStringg.apply(5));
+
+        // Creamos un ArraList y llenamos con nombres aleatorios
+        ArrayList<String> nombres = new ArrayList<>();
+        nombres.add("Arturo");
+        nombres.add("Katya");
+        nombres.add("Jenny");
+
+        Stream<String> valores = nombres.stream().map(x -> x.toUpperCase());
+        valores.forEach(x -> System.out.println(x));
     }
     public static String diHola(String nombre) {
         return "Hola: " + nombre;
