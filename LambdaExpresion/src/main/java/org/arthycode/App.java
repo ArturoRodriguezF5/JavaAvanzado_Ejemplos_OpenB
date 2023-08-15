50df3556ebdb034b810bc967c1880793c0f3d966
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-08-14 19:07:11 -0600

    Commit para actualizar cambios.

--- LambdaExpresion/src/main/java/org/arthycode/App.java
+++ LambdaExpresion/src/main/java/org/arthycode/App.java
@@ -29,9 +29,23 @@ public class App {
             System.out.println(elemento);
         }
     }
+
+    public void calcular() {
+        /*Operacion operacion = new Operacion() {
+            @Override
+            public double calcularPromedio(double n1, double n2) {
+                return (n1 + n2) / 2;
+            }
+        };
+        System.out.println(operacion.calcularPromedio(2, 3));*/
+
+        Operacion operacion = (double x, double y) -> (x + y) / 2;
+        System.out.println(operacion.calcularPromedio(2, 3));
+    }
     public static void main( String[] args ) {
+
         App app = new App();
         app.ordenar();
-
+        app.calcular();
     }
 }
---
+++ LambdaExpresion/src/main/java/org/arthycode/Operacion.java
@@ -0,0 +1,6 @@
+package org.arthycode;
+
+public interface Operacion {
+
+    double calcularPromedio(double n1, double n2);
+}
