9ed3fab967cb343edbf5957506c464a4b8e0b7ef
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-08-25 19:20:55 -0600

    Commit para actualizar cambios.

--- LambdaExpresion/src/main/java/org/arthycode/methodReference/MeRefApp.java
+++ LambdaExpresion/src/main/java/org/arthycode/methodReference/MeRefApp.java
@@ -1,13 +1,26 @@
 package org.arthycode.methodReference;
 
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MeRefApp {
     public static void referenciarMetodoStatic() {
         System.out.println("Método referido static");
     }
 
     public void referenciarMetodoInstanciaObjetoArbitrario() {
-
+        String[] nombres = {"Mito", "Code", "Jaime"};
+        /*Arrays.sort(nombres, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareToIgnoreCase(o2);
+            }
+        });*/
+        /*Arrays.sort(nombres, (s1, s2) -> s1.compareToIgnoreCase(s2));
+        System.out.println(Arrays.toString(nombres));*/
+        Arrays.sort(nombres, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(nombres));
     }
     public void referenciarMetodoInstanciaObjetoParticular() {
 
@@ -17,12 +30,19 @@ public class MeRefApp {
     }
 
     public void operar() {
-        Operacion op = () -> MeRefApp.referenciarMetodoStatic();
-        op.saludar();
+        /*Operacion op = () -> MeRefApp.referenciarMetodoStatic();
+        op.saludar();*/
+
+        Operacion op2 = MeRefApp::referenciarMetodoStatic;
+        op2.saludar();
+
+
+
     }
 
     public static void main(String[] args) {
         MeRefApp app = new MeRefApp();
         app.operar();
+        app.referenciarMetodoInstanciaObjetoArbitrario();
     }
 }
