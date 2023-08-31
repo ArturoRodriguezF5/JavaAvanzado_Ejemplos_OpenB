c01b390c403365acb4fc3315214b6b54ceeb3ec9
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-08-30 21:45:09 -0600

    Commit para actualizar cambios.

--- LambdaExpresion/src/main/java/org/arthycode/forEach_removelf_sort/forEach.java
+++ LambdaExpresion/src/main/java/org/arthycode/forEach_removelf_sort/forEach.java
@@ -1,4 +1,25 @@
 package org.arthycode.forEach_removelf_sort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class forEach {
+
+    public static void main(String[] args) {
+
+        List<String> paises = new ArrayList<>();
+        paises.add("Mexico");
+        paises.add("Canada");
+        paises.add("France");
+        paises.add("Germany");
+        paises.add("United States");
+
+        paises.forEach(System.out::println);
+
+        System.out.println(paises);
+        paises.removeIf(x -> x.equals("France"));
+        System.out.println(paises);
+        paises.sort(String::compareTo);
+        paises.forEach(System.out::print);
+    }
 }
