7de52d7dc6f57e3076c1fd953fe04ed9b03806b0
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-09-03 16:22:32 -0600

    Commit para actualizar cambios.

---
+++ LambdaExpresion/src/main/java/org/arthycode/streamApi/StreamsApp.java
@@ -0,0 +1,51 @@
+package org.arthycode.streamApi;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Locale;
+
+public class StreamsApp {
+
+    private List<String> lista;
+    private List<String> numeros;
+
+    public StreamsApp(){
+        lista =  new ArrayList<String>();
+        lista.add("Mito");
+        lista.add("Code");
+        lista.add("Jaime");
+        lista.add("MitCode");
+
+        numeros = new ArrayList<String>();
+        numeros.add("1");
+        numeros.add("2");
+    }
+    public void filtrar() {
+        lista.stream().filter(x -> x.startsWith("M")).forEach(System.out::println);
+    }
+    public void ordenar() {
+        //lista.stream().sorted().forEach(System.out::println);
+        lista.stream().sorted((x, y) -> y.compareTo(x)).forEach(System.out::println);
+    }
+    public void transformar() {
+        //lista.stream().map(String::toUpperCase).forEach(System.out::println);
+        numeros.stream().map(x -> Integer.parseInt(x) + 1).forEach(System.out::println);
+    }
+    public void limitar() {
+        lista.stream().limit(2).forEach(System.out::println);
+    }
+    public void contar() {
+        long x = lista.stream().count();
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        StreamsApp app = new StreamsApp();
+        //app.filtrar();
+        //app.ordenar();
+        //app.transformar();
+        //app.limitar();
+        app.contar();
+    }
+}
